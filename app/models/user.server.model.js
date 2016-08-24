df4f57ee9b997baaa801fe9ae7f05523fc009fe2
Matthew <pickledyamsman@gmail.com>
2016-08-24 16:24:02 -0400

    big changes to user.server.model.js

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,12 +1,13 @@
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    crypto = require('crypto'),
+    Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
   firstName: String,
   lastName: String,
   email: {
     type: String,
-    index: true, // allows email to be indexed, useful if it will be queried a lot
+    //index: true,- allows email to be indexed, useful if it will be queried a lot
     match: /.+\@.+\..+/ //makes sure email matches regrex name@email.com
   },
   username: {
@@ -19,30 +20,76 @@ var UserSchema = new Schema({
     type: String,
     validate: [ // checks condition length >= 6 if false sends error message
       function(password) {
-        return password.length >= 6;
-      },
-      'Password should be longer'
+        return password && password.length >= 6;
+      }, 'Password should be longer'
     ]
   },
+  salt: { // hashes password
+    type: String
+  },
+  provider: { // indicate strategy used to register the user
+    type: String,
+    required: 'Provider is required'
+  },
+  providerId: String, // indicates user identifier for authentication strategy
+  providerData: {}, // store user object retrieved from OAuth provider
   created: {
     type: Date,
     default: Date.now
-  },
-  website: {
-    type: String,
-    get: function(url) { // assigns http:// to url if not assigned
-      if (!url) {
-        return url;
-      } else{
-        if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
-          url = 'http://' + url;
-        }
-
-        return url;
-        }
-    }
   }
 });
 
+UserSchema.virtual('fullName').get(function() {
+  return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+  var splitName = fullName.split(' ');
+  this.firstName = splitName[0] || '';
+  this.lastName = splitName[1] || '';
+});
+
+UserSchema.pre('save', function(next) { // hashes users' passwords
+  if (this.password) {
+    this.salt = new
+      Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+    this.password = this.hashPassword(this.password);
+  }
+
+  next();
+});
+
+UserSchema.methods.hashPassword = function(password) { // replaces password with hashed version
+  return crypto.pbkdf2Sync(password, this.salt, 10000,
+    64).toString('base64');
+};
+
+UserSchema.methods.authenticate = function(password) { // accepts string, hashes it, compares to hashed password
+  return this.password === this.hashPassword(password);
+};
+
+UserSchema.statics.findUniqueUsername = function(username, suffix,
+  callback) { // finds available unique username for new users
+  var _this = this;
+  var possibleUsername = username + (suffix || '');
+
+  _this.findOne({
+    username: possibleUsername
+  }, function(err, user) {
+    if (!err) {
+      if (!user) {
+        callback(possibleUsername);
+      } else {
+        return _this.findUniqueUsername(username, (suffix || 0) +
+          1, callback);
+      }
+    } else {
+      callback(null);
+    }
+  });
+};
+
+UserSchema.set('toJSON', {
+  getters: true,
+  virtuals: true
+});
+
 mongoose.model('User', UserSchema);
-UserSchema.set('toJSON', { getters: true });
