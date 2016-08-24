124e8816c5dbc68065c542b311c0591af070e1d7
Matthew <pickledyamsman@gmail.com>
2016-08-24 17:14:16 -0400

    finished chapter 6, local login signup, oauth

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,19 @@
 module.exports = {
   db: 'mongodb://localhost/mean-book',
-  sessionSecret: 'developmentSessionSecret'
+  sessionSecret: 'developmentSessionSecret',
+  facebook: {
+    clientID: 'Application Id',
+    clientSecret: 'Application Secret',
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
+  },
+  twitter: {
+    clientID: 'Application Id',
+    clientSecret: 'Application Secret',
+    callbackURL: 'http://localhost:3000/oauth/twitter/callback'
+  },
+  google: {
+    clientID: 'Application Id',
+    clientSecret: 'Application Secret',
+    callbackURL: 'http://localhost:3000/oauth/google/callback'
+  }
 };
